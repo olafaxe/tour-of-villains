15e51bc5ecc80c89c57d696ccd3ae40a51d930d1
Ola Faxe <olafaxe@gmail.com>
2019-12-09 07:29:34 +0100

    security risk upd.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,13 @@ import { FormsModule } from "@angular/forms"; // <-- NgModel lives here
 import { HttpClientModule } from "@angular/common/http";
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 import { InMemoryDataService } from "./in-memory-data.service";
-
 import { AppComponent } from "./app.component";
 import { VillainsComponent } from "./villains/villains.component";
 import { VillainDetailComponent } from "./villain-detail/villain-detail.component";
 import { MessagesComponent } from "./messages/messages.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { DashboardComponent } from "./dashboard/dashboard.component";
-import { VillainSearchComponent } from './villain-search/villain-search.component';
+import { VillainSearchComponent } from "./villain-search/villain-search.component";
 
 @NgModule({
   declarations: [
